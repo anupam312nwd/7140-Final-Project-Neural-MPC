597e83ae9f39852fe844c5741fc1be6e0aa7f13d
luke1 <luke1@cadence.com>
2020-02-03 12:40:05 -0500

    Test file from chat today

--- test/gym_test.py
+++ test/gym_test.py
@@ -1,17 +1,35 @@
 import gym
 from tqdm import tqdm 
+import matplotlib.pyplot as plt
+
+# from rnn-agent.rnn import MyNetwork
+# from node-agent.node import MyNetwork2
 
 env = gym.make("Acrobot-v1")
 observation = env.reset()
 
-pbar = tqdm(range(1000))
-for _ in pbar:
-  env.render()
-  action = env.action_space.sample() # your agent here (this takes random actions)
-  observation, reward, done, info = env.step(action)
+# obs_shape = env.observation_space.shape
+# action_shape = env.action_space.n
 
-  pbar.set_description(f"Reward: {reward}")
+# agent = NeuralMPC(config) / NeuralRNN(config) # running the agent
+# net = RNN(state_shape) # running the network
+for _ in range(5):
+  # action = env.action_space.sample() # your agent here (this takes random actions)
+  # prediction = net(observation, action) # network takes in the current image, predicts next one 
+  # action = agent.act(observation) # your agent here (this takes random actions)
+
+  _, reward, done, info = env.step(action)
+  observation = env.render(mode='rgb_array')
+
+  # agent.update(observation) # In train mode
+  # net.fit(loss(prediction, observation)) # Update your neural net, with the new observation
+
+  print(f"{observation}, {action}, {reward}, {done}")
 
   if done:
     observation = env.reset()
+plt.imshow(observation)
+plt.show()
+
+
 env.close()
