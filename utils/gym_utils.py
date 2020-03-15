6c50c824114b027101e2beb6b679c48df0fdb512
luke <lukeroberto213@gmail.com>
2020-03-14 22:57:27 -0400

    First pass at NeuralODE agent, still having issues with convergence.

--- utils/gym_utils.py
+++ utils/gym_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def generate_training_data(env, samples, filename=None):
+def generate_training_data(env, samples, horizon=1, filename=None):
     """Generates training data in the form of numpy array's for state, action, and next state datasets
 
     Arguments:
@@ -11,25 +11,26 @@ def generate_training_data(env, samples, filename=None):
     """
 
     states = np.zeros((samples, *env.observation_space.shape))
-    actions = np.zeros((samples, 1))
-    next_states = np.zeros((samples, *env.observation_space.shape))
+    actions = np.zeros((samples, horizon))
+    next_states = np.zeros((samples, horizon, *env.observation_space.shape))
 
-    state = env.reset()
     for s in range(samples):
-        action = env.action_space.sample()
-        
-        # Collect state and action
-        states[s] = state
-        actions[s] = action
+        state = env.reset()
+        for i in range(horizon):
+            action =  env.action_space.sample()
+            
+            # Collect state and action
+            states[s] = state
+            actions[s, i] = action
 
-        next_state, _, done, _ = env.step(action)
+            next_state, _, done, _ = env.step(action)
 
-        # Collect state and action
-        next_states[s] = next_state
+            # Collect final state
+            next_states[s, i] = next_state
 
-        state = next_state
-        if done:
-            env.reset()
+            state = next_state
+            if done:
+                env.reset()
     
     if filename:
         np.save(filename + "_states.npy", states)
