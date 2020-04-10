ee44be8e3cb36edbc434c799af66ff96ef389189
anupam312nwd <anupam312nwd@users.noreply.github.com>
2020-04-10 13:34:56 -0400

    replaced vGRU class by importing it

--- agents/env_model_compare.py
+++ agents/env_model_compare.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
+from utils.gym_utils import generate_training_data
+from test_torchgru import vGRU
 # %matplotlib inline
 
 # data_states = np.load('data/trial1_states.npy')
@@ -26,27 +28,6 @@ import matplotlib.pyplot as plt
 
 # print(len(train_data), len(test_data))
 
-class vGRU(nn.Module):
-
-    def __init__(self, input_size= 7, hidden_size= 30, out_size= 6):
-
-        super().__init__()
-
-        self.hidden_size = hidden_size
-        # Add an vGRU layer
-        self.gru = nn.GRU(input_size, hidden_size)
-
-        # Add a fully-connected layer
-        self.linear = nn.Linear(hidden_size,out_size)
-
-        # Initialize h0
-        self.hidden = torch.zeros(1, 1, hidden_size)
-
-    def forward(self, seq):
-        gru_out, self.hidden = self.gru(seq.reshape(-1, 1, len(seq)), self.hidden)
-        pred = self.linear(gru_out.reshape(1,-1))
-        return pred[-1]   # we only care about the last prediction
-
 
 model = vGRU()
 print(model)
@@ -56,14 +37,11 @@ optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
 model.load_state_dict(torch.load('node_agent/sample_torchgru.pt'))
 model.eval()
 
-
-from utils.gym_utils import generate_training_data
-
 env = gym.make("Acrobot-v1")
 states, actions, next_states = generate_training_data(env, 100)
 
 print(states, actions, next_states)
-input_data = torch.from_numpy(np.concatenate((states, actions), axis = 1))
+input_data = torch.from_numpy(np.concatenate((states, actions), axis=1))
 output_data = torch.from_numpy(next_states)
 
 N = input_data.shape[0]
@@ -76,7 +54,6 @@ for i in range(N):
 print(len(data))
 
 
-
 y_env = output_data
 y_model = torch.Tensor(y_env.shape)
 
@@ -86,11 +63,11 @@ with torch.no_grad():
 
         seq = seq.float()
         if i > 0: seq[:6] = y_pred
-        model.hidden = (torch.zeros(1,1,model.hidden_size))
+        model.hidden = (torch.zeros(1, 1, model.hidden_size))
 
         y_pred = model(seq)
         y_model[i] = y_pred
-        if i==1: print(seq.shape, y_pred.shape, seq[:6], y_pred)
+        if i == 1: print(seq.shape, y_pred.shape, seq[:6], y_pred)
         # print(y_pred, y_train)
 
 print('----------------')
@@ -100,24 +77,16 @@ print(y_env)
 print('----------------')
 print(type(y_model))
 print(y_env.shape)
-print(y_env[:,1])
+print(y_env[:, 1])
 
-plt.plot(y_env[:,0], 'b', marker= "o", label= "True")
-plt.plot(y_model[:,0], 'bx', label= "RNN")
+plt.plot(y_env[:, 0], 'b', marker="o", label="True")
+plt.plot(y_model[:, 0], 'bx', label="RNN")
 
-plt.plot(y_env[:,1], 'g', marker= "o", label= "True")
-plt.plot(y_model[:,1], 'gx', label= "RNN")
-
-# plt.plot(y_env[:,1])
-# plt.plot(y_model[:,1])
+plt.plot(y_env[:, 1], 'g', marker="o", label="True")
+plt.plot(y_model[:, 1], 'gx', label="RNN")
 
 # plt.plot(y_env[:,2])
 # plt.plot(y_model[:,2])
-
-# plt.plot(y_env[:,3])
-# plt.plot(y_model[:,3])
-
-
 
 plt.legend()
 # plt.savefig('Environment_GRU_compare.png')
